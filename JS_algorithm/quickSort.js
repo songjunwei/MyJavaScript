c802ba0de939f5facbfa0a845104e2a0b2f12360
songjunwei <song.junwei@iwhalecloud.com>
2019-04-03 21:24:26 +0800

    arr.splice

--- JS_algorithm/quickSort.js
+++ JS_algorithm/quickSort.js
@@ -10,7 +10,7 @@ function quickSort( arr ){ // O(n log n)
  let left = [];
  let right = [];
   // split会对原有数组操作，返回删除部分的数组，第一个参数为删除的位置，第二个为删除的个数
- const interValue = arr.split(interleval, 1)[0];
+ const interValue = arr.splice(interleval, 1)[0];
  for(let i = 0; i < arr.length; i++){
    if( arr[i] < interValue){
     left.push(arr[i]);
